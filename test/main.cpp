60438162395fbfefd88010ce3fc9e37b9879a433
Veit Heller <veit.heller@student.htw-berlin.de>
2015-01-20 11:35:36 +0100

    Updated test main to smart pointers; BackendTest fails

--- test/main.cpp
+++ test/main.cpp
@@ -14,8 +14,8 @@
  * Implements a name/factory function pair.
  */
 struct Tests{
-    QString *name;
-    QObject *(*testclass)();
+    QString name;
+    std::shared_ptr<QObject> (*testclass)();
 };
 
 /**
@@ -25,8 +25,8 @@ struct Tests{
  * Factory function that creates a templated object.
  */
 template<typename T>
-QObject* factory(){
-    return new T();
+auto factory() -> std::shared_ptr<QObject> {
+    return std::shared_ptr<T>(new T());
 }
 
 /**
@@ -45,53 +45,50 @@ int main(int argc, char** argv){
     int status = 0, statusSum = 0;
 
     struct Tests testcases[] = {
-            {new QString("CodeEditor"), factory<CodeEditorTest>},
-            {new QString("EditorWindow"), factory<EditorWindowTest>},
-            {new QString("Backend"), factory<BackendTest>},
-            {new QString("SettingsBackend"), factory<SettingsBackendTest>},
-            {new QString("Renderer"), factory<RendererTest>},
-            {new QString("CodeHighlighter"), factory<CodeHighlighterTest>}
+            {QStringLiteral("CodeEditor"), factory<CodeEditorTest>},
+            {QStringLiteral("EditorWindow"), factory<EditorWindowTest>},
+            {QStringLiteral("SettingsBackend"), factory<SettingsBackendTest>},
+            {QStringLiteral("Renderer"), factory<RendererTest>},
+            {QStringLiteral("Backend"), factory<BackendTest>},
+            {QStringLiteral("CodeHighlighter"), factory<CodeHighlighterTest>}
             };
 	    
     unsigned int size = sizeof(testcases)/sizeof(Tests);
-    int* codes = new int[size]();
+    std::unique_ptr<int[]> codes(new int[size]);
 
-    auto testRun = [&status, &statusSum, &codes](QObject* testObject, QString* name, int i) {
+    auto testRun = [&status, &statusSum, &codes](QObject* testObject, QString name, int i) {
                         try{
                             status = QTest::qExec(testObject, 0, 0);
                         }catch(QException e){
-                            qDebug() << *name << "Test threw QException:" << e.what();
+                            qDebug() << name << "Test threw QException:" << e.what();
                         }catch(const std::exception& e){
-                            qDebug() << *name << "Test threw STL Exception:" << e.what();
+                            qDebug() << name << "Test threw STL Exception:" << e.what();
                         }
                         if(status != 0){
                             codes[i-1] = status;
-                            statusSum += codes[i-1];
+                            statusSum += status;
                             status = 0;
                         }
                         qDebug() << "";
-                        delete testObject;
                     };
 
     if(argc == 1 || QString(argv[1]) == "runall")
         for(unsigned int i = 0; i < size; i++)
-            testRun(testcases[i].testclass(), testcases[i].name, i);
+            testRun((testcases[i].testclass()).get(), testcases[i].name, i);
     else
         for(int i = 1; i < argc; i++){
             for(struct Tests &t: testcases)
-                if(argv[i] == *t.name){
-                    testRun(t.testclass(), t.name, i-1);
+                if(argv[i] == t.name){
+                    testRun((testcases[i].testclass()).get(), t.name, i-1);
                     break;
                 }
         }
 
     for(unsigned int i = 0; i < size; i++)
         if(codes[i] != 0)
-            qDebug() << "Testclass" << *(testcases[i+1].name) << "failed" << codes[i] << "test(s).";
+            qDebug() << "Testclass" << (testcases[i+1].name) << "failed" << codes[i] << "test(s).";
     qDebug() << "Ran tests for" << size << "classes.";
     qDebug() << "Total failed tests:" << statusSum;
-
-    delete[] codes;
 
     return statusSum;
 }
